0b99f96293254cbc517eff35373dae65b861bb3c
NielsWD <niels.waughray-dupoux@epitech.eu>
2017-06-12 12:03:30 +0200

    Cleaning

--- app/src/main/java/nielswd/wazappcalculator/CalculatorDisplay.java
+++ app/src/main/java/nielswd/wazappcalculator/CalculatorDisplay.java
@@ -16,14 +16,14 @@ import android.widget.GridLayout;
 import java.util.ArrayList;
 
 public class CalculatorDisplay extends AppCompatActivity {
-    private EditText displayEditText;
-    private GridLayout numbersGrid;
-    private GridLayout opGrid;
-    private String[] numbersList = new String[] {"7","8","9","4","5","6","1","2","3",".","0","="};
-    private String[] opList = new String[] {"/","*","-","+"};
-    private float firstNumber;
-    private float secondNumber;
-    private String operation;
+    private EditText        displayEditText;
+    private GridLayout      numbersGrid;
+    private GridLayout      opGrid;
+    private String[]        numbersList = new String[] {"7","8","9","4","5","6","1","2","3",".","0","="};
+    private String[]        opList = new String[] {"/","*","-","+"};
+    private float           firstNumber;
+    private float           secondNumber;
+    private String          operation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +35,6 @@ public class CalculatorDisplay extends AppCompatActivity {
         initComponents();
         addNumbers();
         addOperations();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     private void addOperations() {
@@ -133,19 +124,14 @@ public class CalculatorDisplay extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_calculator_display, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
